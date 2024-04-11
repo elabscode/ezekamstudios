8c8cc32be3f0d60dbf5a15e6b007f083f5b32026
Devis Kobby <deviskobby@gmail.com>
2024-04-11 16:22:54 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <title>Ezekam Studios</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="logo-wrapper"><img src="images/ezekam_logo.png" alt="Logo"></div>
+    <h1>Our site is coming soon</h1>
+</body>
+</html>
